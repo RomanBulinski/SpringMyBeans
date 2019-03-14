bc7191fdad717bdcecfbe41fb234395fa56971e6
Roman Bulinski <aboandrus@gmail.com>
2019-03-14 09:29:58 +0100

    Implemented beans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import implementation.A;
+import implementation.AB;
 import implementation.B;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,13 +16,18 @@ public class Main {
 //        a.printData();
 
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        A a = context.getBean("pierwszy_bean", A.class);
-        a.printData();
-        A a2 = context.getBean("drugi_bean", A.class);
-        a2.printData();
+//        A a = context.getBean("pierwszy_bean", A.class);
+//        a.printData();
+//        A a2 = context.getBean("drugi_bean", A.class);
+//        a2.printData();
+//
+//        B b = context.getBean("trzeci_bean", B.class);
+//        b.printData();
 
-        B b = context.getBean("trzeci_bean", B.class);
-        b.printData();
+        AB ab = context.getBean("AB_bean", AB.class);
+        ab.getA().printData();
+        ab.getB().printData();
+
 
     }
 }
---
+++ src/main/java/implementation/AB.java
@@ -0,0 +1,20 @@
+package implementation;
+
+public class AB {
+
+    A a;
+    B b;
+
+    public AB(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+}
